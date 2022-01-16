f5e5b870a50efa59d8f43b23aa1d82bd070cef50
lucas <lucas_gomes18@hotmail.com>
2022-01-16 12:51:46 -0300

    Alteracao da img para imagem e copiar e colar o index e script

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
+
+const imagem = document.querySelector('#imagem');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
+
+const imagem1 = document.querySelector('#imagem1');
+const nomeDoPersonagem1 = document.querySelector('#nome1');
+const especie1 = document.querySelector('#especie1');
+const condicao1 = document.querySelector('#status1');
+
+const imagem2 = document.querySelector('#imagem2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
 
 gerarValorAleatorio =() =>{
     return Math.floor(Math.random() * 671);
@@ -10,7 +21,10 @@ gerarValorAleatorio =() =>{
 
 pegarPersonagem = () => {
     let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    let numeroAleatorio1 = gerarValorAleatorio();
+    let numeroAleatorio2 = gerarValorAleatorio();
+
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
@@ -22,6 +36,34 @@ pegarPersonagem = () => {
         nomeDoPersonagem.innerHTML = data.name;
         especie.innerHTML = data.species;
         condicao.innerHTML =data.status;
+    });
+
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio1}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem1.src = data.image;
+        imagem1.alt =data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
+        especie1.innerHTML = data.species;
+        condicao1.innerHTML =data.status;
+    });
+
+    fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio2}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem2.src = data.image;
+        imagem2.alt =data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML =data.status;
     });
 }
 
